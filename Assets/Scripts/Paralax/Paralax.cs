784bda52c7618b6cf301baf14dcc8712006f444e
Tommix1403 <sachabrault08@gmail.com>
2026-01-29 23:29:08 +0100

    Tjr paralax

--- Assets/Scripts/Paralax/Paralax.cs
+++ Assets/Scripts/Paralax/Paralax.cs
@@ -1,6 +1,18 @@
 using UnityEngine;
 
-public class Parala
+public class Paralax : MonoBehaviour
 {
-    
+    private float _start;
+    public float _speed;
+    void Start()
+    {
+        _start = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float change = Camera.main.transform.position.x * _speed;
+        transform.position = new Vector3(_start + change, transform.position.y, transform.position.z);
+    }
 }
